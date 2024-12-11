623079796d4ba7f1d697ea92f8820bf09cf98ccd
allen <74865251a@gmail.com>
2024-12-11 11:53:58 +0800

    add solana wallet package, add fucntion

---
+++ FrontEnd/dapp/src/utils/transaction.tsx
@@ -0,0 +1,28 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program, BN, AnchorProvider } from "@coral-xyz/anchor";
+import {
+  PublicKey,
+  Connection,
+} from "@solana/web3.js";
+import { AnchorWallet } from "@solana/wallet-adapter-react";
+
+export const getProgram = (
+  connection: Connection,
+  wallet: AnchorWallet,
+  idl: any,
+  contractAddress: string,
+  authority: PublicKey, // Add authority parameter
+) => {
+  const provider = new AnchorProvider(connection, wallet, {
+    commitment: "confirmed",
+  });
+  anchor.setProvider(provider);
+
+  const program: Program<programType> = new Program(
+    idl as any,
+    contractAddress,
+    provider,
+  );
+
+  return { program, provider, authority }; // Return authority if needed
+};

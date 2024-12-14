07919500030eb54b80642d5fd7cf929068ca386e
allen <74865251a@gmail.com>
2024-12-14 12:01:32 +0800

    add new config

--- FrontEnd/dapp/src/config/config.ts
+++ FrontEnd/dapp/src/config/config.ts
@@ -1,9 +1,9 @@
 export const CONFIG = {
-    programId: "27i99mTmMMiZp3ArDwgo9aT5UqMU9SgURKSHw56rvaZf",
+    programId: "CNq5h7p1fmE4rPNPBsUMtxJfELkfff88ofwCEgFoN1bW",
     mintToken: "4QD4Sb9N9q98mRjBJnbv1D2BUgRRmeHwrvhY6X8F9zjn",
     ownerTokenAccount: "AwYrBkskmz8xYH6kfKw81tZJLoBiMtjT53DuJQL1oQtu",
     tokenProgram: "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
-    tokenPool: "F4msAHR36XETYw7kspgrDawiRFeieUfx1cCaw1Sxc5Lu",
+    tokenPool: "DcojQmnVaaz9njMfVnqeoi35X4re7W8cHrVWnvMtGu1F",
     derivedToken: {
       address: "4Aj8nehaBiP8WNxzqaPTfu35vEhzG6r2dmHssqyBvpE3",
       derivedTokenAddress: "CaQqLf5faSgMBCwhjwbaVwWbe1A9op9m2gT9yu4YGxg",
